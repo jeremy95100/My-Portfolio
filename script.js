11bd5a3eaeda17e58531d6f24817c10930e06253
jeremy95100 <jeremy_am@hotmail.fr>
2024-06-17 13:22:11 +0200

    changement code

--- script.js
+++ script.js
@@ -1,72 +1,153 @@
+const pageTurnBtn  = document.querySelectorAll('.nextprev-btn');
 
-    const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
+pageTurnBtn.forEach((el, index) => {
+    el.onclick = () => {
+        const pageTurnId = el.getAttribute('data-page');
+        const pageTurn = document.getElementById(pageTurnId);
 
+        if (pageTurn.classList.contains('turn') ) {
+            pageTurn.classList.remove('turn');
+            setTimeout(function(){
+                pageTurn.style.zIndex = 20 - index;
+            }, 500);
+         }
+         else {
+            pageTurn.classList.add('turn');
+            setTimeout(function(){
+                pageTurn.style.zIndex = 20 + index;
+            }, 500);
+         }
+             }
+})
 
-    pageTurnBtn.forEach((el, index) => {
-        el.onclick = () => {
-            const pageTurnId = el.getAttribute('data-page');
-            const pageTurn = document.getElementById(pageTurnId);
+const pages = document.querySelectorAll('.book-page.page-right');
+const ContactMeBtn = document.querySelector('.btn.contact-me');
 
-            if (pageTurn.classList.contains('turn')) {
-                pageTurn.classList.remove('turn');
-                setTimeout(() => {
-                    pageTurn.style.zIndex = 20 - index;
-                }, 500);
-            } 
-            else {
-                pageTurn.classList.add('turn');
-                setTimeout(() => {
-                    pageTurn.style.zIndex = 20 + index;
-                }, 500);
-            }
-        };
-    });
+ContactMeBtn.onclick = () => {
+    pages.forEach((page, index) => {
+        setTimeout(function(){
+            page.classList.add('turn');
 
-    const pages= document.querySelectorAll('.book-page.page-right');;
-    const contactMeBtn = document.querySelector('.btn.contact-me');
+             setTimeout(function(){
+                page.style.zIndex = 20 + index;
+            }, 500);
+        }, (index + 1) * 200 + 100);
+    })
+}
 
-    contactMeBtn.onclick = () => {      //contact me button
-        pages.forEach((page, index) => {
-            setTimeout(() => {
-                page.classList.add('turn');
-                
-                setTimeout(() => {
-                    page.style.zIndex = 20 + index;
-                }, 500);
-            }, (index + 1) * 200 + 100);
-        })
+let totalPages = pages.length;
+let pageNumber = 0;
+
+function reverseIndex() {
+     pageNumber--;
+     if (pageNumber < 0) {
+        pageNumber = totalPages - 1;
+     
     }
+}
 
-    let totalPages = pages.length;
-    let pageNumber = totalPages;
+const backProfileBtn = document.querySelector('.back-profile');
 
-    function reverseIndex() {
-        pageNumber--;
-        if (pageNumber < 0) {
-            pageNumber = totalPages - 1;
-        }
-    }
+backProfileBtn.onclick = () => {
+    pages.forEach((_, index) => {
+        setTimeout(function(){
+            reverseIndex();
+            pages[pageNumber].classList.remove('turn');
 
-    
-
-
-
-    const backProfileBtn = document.querySelector('.back-profile');
-
-    backProfileBtn.onclick = () => {
-        pages.forEach((_, index) => {
             setTimeout(() => {
                 reverseIndex();
-                pages[pageNumber].classList.remove('turn');
+                pages[pageNumber].style.zIndex = 10 + index;
+            }, 500)
 
-                setTimeout(() => {
-                    reverseIndex();
-                    pages[pageNumber].style.zIndex = 10 + index;
-                }, 500);
-            }, (index + 1) * 200 + 100);
+            }, (index + 1) * 200 + 100)
+    })
+}     
 
-        }
-        )
-    }
+const coverRight = document.querySelector('.cover.cover-right');
+const pageLeft = document.querySelector('.book-page.page-left');
 
+setTimeout(() => {
+    coverRight.classList.add('turn');
+}, 2100)
+
+
+setTimeout(() => {
+    coverRight.style.zIndex = -1;
+}, 2800)
+
+setTimeout(() => {
+    pageLeft.style.zIndex = 20;
+}, 3200)
+
+pages.forEach((_, index) => {
+    setTimeout(() => {
+        reverseIndex();
+        pages[pageNumber].classList.remove('turn');
+
+        setTimeout(() => {
+            reverseIndex();
+            pages[pageNumber].style.zIndex = 10 + index;
+        }, 500)
+
+        }, (index + 1) * 200 + 2100)
+})
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
